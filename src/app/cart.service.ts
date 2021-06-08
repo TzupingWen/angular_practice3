a79c450233bf18a08ea7ca8120e6046291c83964
TzupingWen <pw.rinya@gmail.com>
2021-06-08 10:19:46 +0800

    add 6/8
    fix cart

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -16,7 +16,16 @@ export class CartService {
     @Output() event = new EventEmitter();
 
     addToCart(product) {
-        this.items.push(product);
+        // 加入購物車的商品要給一物件，待解決:檢查是否有重複商品
+        this.items.push({
+            // 定義新物件的商品與數量
+            product: product,
+            qty: 1,
+        });
+
+
+        // 原先push的product沒有定義新物件，[(ngModel)]會一直共用同個id
+        // this.items.push(product);
     }
 
     getItems() {
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -9,74 +9,60 @@ import { products } from '../products';
 })
 export class CartComponent implements OnInit {
   items = this.cartService.getItems();
-  qty:number;
-  subPrice:number;
-  totalQty:number;
+  // qty:number;
+  // subPrice:number;
+  // totalQty:number;
+  // totalPrice:number=0;
+
+  // 數量
+  // subQty(){
+  //   this.qty = 0;
+  //   if(this.items){
+  //     this.qty += 1;
+  //   }
+  // }
+
   totalPrice:number;
-  
 
-  // 購物車數量
-  subQty(){
-    this.qty = 0;
-    if(this.items){
-      this.qty += 1;
-      return this.qty;
-    }
-  }
-
-  // 購物車總計
-  sum() {
-    this.subPrice = 0;
-    this.totalQty = 0;
+  // 用get()方法取得總金額的計算；參考get computedName()
+  get computedTotal(){
     this.totalPrice = 0;
-    // if(this.items){
-    //   this.items.map(item => {
-    //     this.subPrice += item.price;
-    //     this.totalQty += this.qty;
-    //     this.totalPrice += item.price * this.qty;
-    //   })
-    // } 
-
-    for(var i=0; i< this.items.length ; i++){
-      this.subPrice += this.items[i].price;
-      this.totalQty += this.qty;
-      this.totalPrice += this.items[i].price * this.qty;
+    for(let i=0; i< this.items.length ; i++){
+      this.totalPrice += (this.items[i].product.price * this.items[i].qty);
     }
-
+    return this.totalPrice;
   }
+
+  // 購物車總計--(無法將所有商品的小計加總)
+  // sum() {
+  //   this.subPrice = 0;
+  //   this.totalQty = 0;
+  //   this.totalPrice = 0;
+  //   for(var i=0; i< this.items.length ; i++){
+  //     this.subPrice += this.items[i].price;
+  //     this.totalQty += this.qty;
+  //     this.totalPrice += this.items[i].price * this.qty;
+  //   }
+
+  // }
 
   // 移除購物車某項商品
   remove(index: number) {
     this.cartService.remove(index);
-    this.subQty();
-    this.sum();
+    // this.subQty();
+    // this.sum();
   }
+
 
   constructor(
     private cartService: CartService
   ) { }
 
-  
-  
   ngOnInit():void {
     console.log(this.cartService.getItems());
     this.items = this.cartService.getItems();
-    this.subQty();
-    this.sum();
-
-    // this.items.forEach(item => {
-    //   this.calcPrice += (item.price * this.qty);
-    // })
-
+    // this.subQty();
+    // this.sum();
   };
-
-
-  // test
-  // x=0;
-  // y=0;
-  // result=0;
-  // calculate(){
-  //   this.result = this.x * this.y;
-  // }
 
 }
