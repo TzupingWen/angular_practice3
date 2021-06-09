04b1363bc8dec033f7a5ad2583333a33ccb7abc0
TzupingWen <pw.rinya@gmail.com>
2021-06-09 17:17:29 +0800

    add 6/9 串接API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-<<<<<<< Updated upstream
-  title = 'Angular Practice';
-=======
   title = 'Angular Practice 2';
 
   constructor() {}
   
->>>>>>> Stashed changes
 }
